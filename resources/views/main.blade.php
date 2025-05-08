6eeceeb98bbb9176ec595ac90fdebaa67a3f4f47
higoomigo <hilalbouti57@gmail.com>
2025-05-09 05:43:28 +0800

    add breeze, landing

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistem Pemetaan Lahan Pertanian &amp; Perkebunan Gorontalo</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="Sistem%20Pemetaan%20Lahan%20Pertanian%20&amp;%20Perkebunan%20Gorontalo_files/css2.css" rel="stylesheet">
     <script src="Sistem%20Pemetaan%20Lahan%20Pertanian%20&amp;%20Perkebunan%20Gorontalo_files/3.4.16"></script>
     <link rel="stylesheet" href="Sistem%20Pemetaan%20Lahan%20Pertanian%20&amp;%20Perkebunan%20Gorontalo_files/all.min.css">
@@ -209,8 +210,18 @@
                 <li><a href="#news" class="nav-link font-semibold text-gray-800 hover:text-primary transition-colors">Berita</a></li>
                 <li><a href="#data" class="nav-link font-semibold text-gray-800 hover:text-primary transition-colors">Sumber Data</a></li>
             </ul>
+            
             <div class="flex items-center gap-4">
-                <button class="bg-primary text-white px-4 py-2 rounded font-semibold hover:bg-primary-dark transition-colors transform hover:-translate-y-0.5 transition-transform ripple">Admin Login</button>
+                @if(Route::has('login'))
+                @auth
+                
+                <a href="{{ url('/dashboard') }}" class="bg-primary text-white px-4 py-2 rounded font-semibold hover:bg-primary-dark transition-colors transform hover:-translate-y-0.5 transition-transform ripple">Dashboard</a>
+                @else
+                @if (Route::has('register'))
+                <a href="{{ route('login') }}" class="bg-primary text-white px-4 py-2 rounded font-semibold hover:bg-primary-dark transition-colors transform hover:-translate-y-0.5 transition-transform ripple">Login</a>
+                @endif
+                @endauth
+                @endif
                 <button class="md:hidden text-primary text-xl" id="hamburgerBtn">
                     <i class="fas fa-bars"></i>
                 </button>
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,23 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ArticleController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('main');
+});
+// Route::get('/profile', [Controller::class, 'edit'])->name('profile.edit');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/deepseek', function () {
-    return view('template_dipsik');
-});
+Route::Resource('articles', ArticleController::class);
 
-Route::resource('articles', ArticleController::class);
+require __DIR__.'/auth.php';
